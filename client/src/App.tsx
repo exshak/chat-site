58a8ca5f0f71ab7b9582a048fe0c4d6fd1983be5
exshak <sbijabhai@gmail.com>
2020-01-12 22:36:50 -0500

    updated client routes with user auth, added auth actions, reducers & types to redux flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core'
 import axios from 'axios'
 import React, { useEffect } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import './App.css'
 import Header from './components/header'
 import HomePage from './pages'
+import PostsPage from './pages/posts'
 import SignInPage from './pages/signin'
 import SignUpPage from './pages/signup'
 import { getCurrentUser } from './redux/actions/userActions'
-import store from './redux/store'
 import defaultTheme from './theme'
 
 const theme = createMuiTheme(defaultTheme)
@@ -18,10 +19,10 @@ if (token) {
   axios.defaults.headers.common['Authorization'] = token
 }
 
-const App: React.FC = () => {
+const App: React.FC = ({ getCurrentUser, currentUser }: any) => {
   useEffect(() => {
-    store.dispatch(getCurrentUser())
-  }, [])
+    getCurrentUser()
+  }, [getCurrentUser])
 
   return (
     <MuiThemeProvider theme={theme}>
@@ -29,12 +30,29 @@ const App: React.FC = () => {
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/signin' component={SignInPage} />
-          <Route exact path='/signup' component={SignUpPage} />
+          <Route exact path='/posts' component={PostsPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() => (currentUser ? <Redirect to='/' /> : <SignInPage />)}
+          />
+          <Route
+            exact
+            path='/signup'
+            render={() => (currentUser ? <Redirect to='/' /> : <SignUpPage />)}
+          />
         </Switch>
       </div>
     </MuiThemeProvider>
   )
 }
 
-export default App
+const mapStateToProps = ({ user: { currentUser } }: any) => ({
+  currentUser
+})
+
+const mapDispatchToProps = (dispatch: any) => ({
+  getCurrentUser: () => dispatch(getCurrentUser())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
