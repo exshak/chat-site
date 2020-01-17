5a166b1f76812d24204a85dba6f492a41ed02544
exshak <sbijabhai@gmail.com>
2020-01-16 23:54:37 -0500

    implemented ability to add/delete comments, like/unlike post, updated dependencies

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -79,6 +79,124 @@ router.get('/posts/:id', auth, async (req, res) => {
   }
 })
 
+// prettier-ignore
+// POST - api/posts/comment/:id - Comment on a post
+router.post('/posts/comment/:id', auth, async (req, res) => {
+  await check('body', 'Text is required').not().isEmpty().run(req)
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  const { id: user, profile: { name, image } } = req.user
+  const { body } = req.body
+
+  try {
+    const post = await Post.findById(req.params.id)
+
+    const newComment = { name, body, image, user }
+
+    post.comments.unshift(newComment)
+
+    await post.save()
+
+    res.json(post.comments)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send({ errors: error.message })
+  }
+})
+
+// prettier-ignore
+// DELETE - api/posts/comment/:id/:comment_id - Delete comment
+router.delete('/posts/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    // Pull out comment
+    const comment = post.comments.find(
+      comment => comment.id === req.params.comment_id
+    )
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ errors: [{ msg: 'Comment does not exist' }] })
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ errors: [{ msg: 'User not authorized' }] })
+    }
+
+    // Get remove index
+    const removeIndex = post.comments
+      .map(comment => comment.id)
+      .indexOf(req.params.comment_id)
+
+    post.comments.splice(removeIndex, 1)
+
+    await post.save()
+
+    res.json(post.comments)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send({ errors: error.message })
+  }
+})
+
+// PUT - api/posts/like/:id - Like post
+router.put('/posts/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    // Check if the post has already been liked
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ errors: [{ msg: 'Post already liked' }] })
+    }
+
+    post.likes.unshift({ user: req.user.id })
+
+    await post.save()
+
+    res.json(post.likes)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send({ errors: error.message })
+  }
+})
+
+// prettier-ignore
+// PUT - api/posts/unlike/:id - Unlike post
+router.put('/posts/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    // Check if the post has not been liked
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length === 0
+    ) {
+      return res.status(400).json({ errors: [{ msg: 'Post has not been liked' }] })
+    }
+
+    // Get remove index
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id)
+
+    post.likes.splice(removeIndex, 1)
+
+    await post.save()
+
+    res.json(post.likes)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send({ errors: error.message })
+  }
+})
+
 module.exports = router
 
 export {}
